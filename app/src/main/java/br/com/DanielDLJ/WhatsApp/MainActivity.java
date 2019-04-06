7c548a5677db435a8afba8e462be060fb9d9e22a
Daniel Leme Júnior <daniel.dlj@hotmail.com>
2019-04-06 19:24:02 -0300

    Toolbar

--- app/src/main/java/br/com/DanielDLJ/WhatsApp/MainActivity.java
+++ app/src/main/java/br/com/DanielDLJ/WhatsApp/MainActivity.java
@@ -2,12 +2,19 @@ package br.com.DanielDLJ.WhatsApp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar =  (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("WhatsApp");
     }
 }
