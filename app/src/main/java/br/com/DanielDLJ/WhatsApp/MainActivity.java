00e5feb0aa425f90cbf9e8a07177db2f298c170b
Daniel Leme Júnior <daniel.dlj@hotmail.com>
2019-04-07 13:56:06 -0300

    LoginActivity, RegisterActivity, SettingsActivity

--- app/src/main/java/br/com/DanielDLJ/WhatsApp/MainActivity.java
+++ app/src/main/java/br/com/DanielDLJ/WhatsApp/MainActivity.java
@@ -1,22 +1,38 @@
 package br.com.DanielDLJ.WhatsApp;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String Tag = "MainActivity";
     private Toolbar mToolbar;
     private ViewPager mVewPager;
     private TabLayout mTabLayout;
     private TabsAccessorAdapter mTabsAccessorAdapter;
 
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        Log.d(Tag,"currentUser.getEmail() = "+currentUser.getEmail());
 
         mToolbar =  (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
@@ -28,5 +44,45 @@ public class MainActivity extends AppCompatActivity {
 
         mTabLayout = findViewById(R.id.main_tabs);
         mTabLayout.setupWithViewPager(mVewPager);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(currentUser == null){
+            SendUserToLoginActivity();
+        }
+    }
+
+    private void SendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(loginIntent);
+    }
+    private void SendUserToSettingsActivity() {
+        Intent settingsIntent = new Intent(MainActivity.this,SettingsActivity.class);
+        startActivity(settingsIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.optios_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.main_logout_options){
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if(item.getItemId() == R.id.main_settings_options){
+            SendUserToSettingsActivity();
+        }
+        if(item.getItemId() == R.id.main_find_friends_options){
+
+        }
+        return true;
     }
 }
