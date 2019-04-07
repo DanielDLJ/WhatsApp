2650686c2487f9586e76c6e0a2c513115d91a455
Daniel Leme Júnior <daniel.dlj@hotmail.com>
2019-04-06 21:22:17 -0300

    Chat, Contact, Group Fragments

--- app/src/main/java/br/com/DanielDLJ/WhatsApp/MainActivity.java
+++ app/src/main/java/br/com/DanielDLJ/WhatsApp/MainActivity.java
@@ -1,5 +1,7 @@
 package br.com.DanielDLJ.WhatsApp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,6 +9,9 @@ import android.support.v7.widget.Toolbar;
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager mVewPager;
+    private TabLayout mTabLayout;
+    private TabsAccessorAdapter mTabsAccessorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
         mToolbar =  (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("WhatsApp");
+
+        mVewPager = findViewById(R.id.main_tabs_pager);
+        mTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        mVewPager.setAdapter(mTabsAccessorAdapter);
+
+        mTabLayout = findViewById(R.id.main_tabs);
+        mTabLayout.setupWithViewPager(mVewPager);
     }
 }
